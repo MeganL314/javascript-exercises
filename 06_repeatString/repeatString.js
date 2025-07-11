7c8da80f4e5970473f1c2dc7f558d4b7372b2ea5
Megan Lynch <megantherese314@gmail.com>
2025-07-11 12:37:13 -0400

    Finish java exercise 06'

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let out_string = "";
+    let i = 0;
+    if(num < 0){
+        return('ERROR')
+    }
+    while(i < num){
+        out_string = out_string + string;
+        i = i + 1;
+    }
+    return out_string;
 };
 
 // Do not edit below this line
