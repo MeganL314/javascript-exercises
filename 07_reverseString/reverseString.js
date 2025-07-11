6129583962da8918fef66a35bc339ecd6d95d895
Megan Lynch <megantherese314@gmail.com>
2025-07-11 12:45:27 -0400

    Finish exercise 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    let output = '';
+    for (let i = array.length - 1; i >= 0; i--){
+        output = output + array[i];
+    }
+    return(output);
 };
 
 // Do not edit below this line
