f557dfa2f1f740f211091f3eb74590d15dc02817
Megan Lynch <megantherese314@gmail.com>
2025-07-17 10:28:11 -0400

    Finish exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    
+    str = str.replace(/[.,!'"\\\s]/g, "");
+    let length = str.length;
 
+    let pal = "";
+    while(length>0){
+        pal = pal+str[length-1];
+        length = length -1;
+    }
+    return pal.toLowerCase() == str.toLowerCase();
 };
 
 // Do not edit below this line
