4b1060848c479091c14ee3f9ba95a8a8cf5ff4e4
Megan Lynch <megantherese314@gmail.com>
2025-07-17 10:50:34 -0400

    Finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let num = Number(input);
+    let seq = [1,1];
 
+    if (num == 0){
+        return 0;
+    }
+    else if (num < 0) {
+        return "OOPS";
+    }
+    let start = 2;
+    let current = 1;
+    while (start < num){
+        current = Number(seq[start-2]) + Number(seq[start-1]);
+        seq.push(current);
+        start = start + 1;
+    }
+    return current;
 };
 
 // Do not edit below this line
